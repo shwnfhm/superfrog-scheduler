2f582358e309d2eaee80efad05cd2b7fa4e7635d
shwnfhm <45961231+shwnfhm@users.noreply.github.com>
2023-04-02 19:43:29 -0500

    more payment info

---
+++ src/backend/java/edu/tcu/cs/superfrogscheduler/payment/PaymentController.java
@@ -0,0 +1,31 @@
+package backend.java.edu.tcu.cs.superfrogscheduler.payment;
+
+import backend.java.edu.tcu.cs.superfrogscheduler.payment.dto.RequestIds;
+import backend.java.edu.tcu.cs.superfrogscheduler.system.Result;
+import backend.java.edu.tcu.cs.superfrogscheduler.system.StatusCode;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PaymentController {
+    private PaymentService paymentService;
+
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment-forms")
+    public Result generatePaymentForms(@RequestBody RequestIds requestIds) {
+        List<Long> selectedIds = requestIds.getRequestIds();
+
+        Period paymentPeriod = requestIds.getPaymentPeriod();
+
+        List<PaymentForm> paymentForms = this.paymentService.generatePaymentForms(selectedIds, paymentPeriod);
+
+        return new Result(true, StatusCode.SUCCESS, "Payment forms are generated successfully.", paymentForms);
+    }
+}
---
+++ src/backend/java/edu/tcu/cs/superfrogscheduler/payment/dto/RequestIds.java
@@ -0,0 +1,37 @@
+package backend.java.edu.tcu.cs.superfrogscheduler.payment.dto;
+
+import backend.java.edu.tcu.cs.superfrogscheduler.payment.Period;
+
+import java.util.List;
+
+public class RequestIds {
+    private List<Long> requestIds;
+
+    private Period paymentPeriod;
+
+
+    public RequestIds() {
+
+    }
+
+    public RequestIds(List<Long> requestIds, Period paymentPeriod) {
+        this.requestIds = requestIds;
+        this.paymentPeriod = paymentPeriod;
+    }
+
+    public List<Long> getRequestIds() {
+        return requestIds;
+    }
+
+    public void setRequestIds(List<Long> requestIds) {
+        this.requestIds = requestIds;
+    }
+
+    public Period getPaymentPeriod() {
+        return paymentPeriod;
+    }
+
+    public void setPaymentPeriod(Period paymentPeriod) {
+        this.paymentPeriod = paymentPeriod;
+    }
+}
