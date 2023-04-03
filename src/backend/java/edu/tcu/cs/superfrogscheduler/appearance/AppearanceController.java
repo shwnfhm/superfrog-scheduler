b7f3ef91f9a5b6bb50f9557f476f835129d9f259
shwnfhm <45961231+shwnfhm@users.noreply.github.com>
2023-04-02 19:53:27 -0500

    appearance controller

--- src/backend/java/edu/tcu/cs/superfrogscheduler/appearance/AppearanceController.java
+++ src/backend/java/edu/tcu/cs/superfrogscheduler/appearance/AppearanceController.java
@@ -2,9 +2,7 @@ package backend.java.edu.tcu.cs.superfrogscheduler.appearance;
 
 import backend.java.edu.tcu.cs.superfrogscheduler.user.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appearance")
@@ -14,8 +12,28 @@ public class AppearanceController {
 
     private UserRepository userRepository;
 
-    @PostMapping("/appearance")
-    public String addRequest(Appearance request){
-        return "success";
+    @PostMapping
+    public String addRequest(Appearance requestedAppearance) {
+        Appearance request = new Appearance(
+
+        );
+        return "Success";
     }
+
+    @PutMapping("/{eventId}")
+    public String approveEvent(@PathVariable("eventId") Long eventId){
+        Appearance appearanceToApprove = appearanceRepository.getOne(eventId);
+        appearanceToApprove.setStatus(AppearanceStatus.APPROVED);
+        appearanceRepository.save(appearanceToApprove);
+        return "Success";
+    }
+
+    @PutMapping("/{eventId}")
+    public String rejectEvent(@PathVariable("eventId") Long eventId){
+        Appearance appearanceToReject = appearanceRepository.getOne(eventId);
+        appearanceToReject.setStatus(AppearanceStatus.REJECTED);
+        appearanceRepository.save(appearanceToReject);
+        return "Success";
+    }
+
 }
--- src/backend/java/edu/tcu/cs/superfrogscheduler/user/UserController.java
+++ src/backend/java/edu/tcu/cs/superfrogscheduler/user/UserController.java
@@ -1,7 +1,9 @@
 package backend.java.edu.tcu.cs.superfrogscheduler.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 }
