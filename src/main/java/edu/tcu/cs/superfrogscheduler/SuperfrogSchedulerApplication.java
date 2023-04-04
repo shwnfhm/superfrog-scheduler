948ef2722b087b33c2c03d977e776359ba11beb4
shwnfhm <45961231+shwnfhm@users.noreply.github.com>
2023-04-04 11:49:17 -0500

    id worker

--- src/main/java/edu/tcu/cs/superfrogscheduler/SuperfrogSchedulerApplication.java
+++ src/main/java/edu/tcu/cs/superfrogscheduler/SuperfrogSchedulerApplication.java
@@ -1,5 +1,6 @@
 package edu.tcu.cs.superfrogscheduler;
 
+import edu.tcu.cs.superfrogscheduler.system.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,9 @@ public class SuperfrogSchedulerApplication {
         SpringApplication.run(SuperfrogSchedulerApplication.class, args);
     }
 
-
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1, 1);
+    }
 
 }
