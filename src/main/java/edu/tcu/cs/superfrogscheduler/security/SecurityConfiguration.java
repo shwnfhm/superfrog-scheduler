a918df75fd216eedb7caee9c2e14157a0fdcc11a
shwnfhm <45961231+shwnfhm@users.noreply.github.com>
2023-04-08 21:45:08 -0500

    login

--- src/main/java/edu/tcu/cs/superfrogscheduler/security/SecurityConfiguration.java
+++ src/main/java/edu/tcu/cs/superfrogscheduler/security/SecurityConfiguration.java
@@ -70,6 +70,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.PUT, this.baseUrl + "/appearances/**").permitAll()
                         .requestMatchers(HttpMethod.DELETE, this.baseUrl + "/appearances/**").permitAll()
                         .requestMatchers(HttpMethod.GET, this.baseUrl + "/appearances/excel").permitAll()
+                        .requestMatchers(HttpMethod.POST, this.baseUrl + "/users/login").permitAll()
                         .requestMatchers(HttpMethod.GET, this.baseUrl + "/users/**").permitAll() // Protect the endpoint.
                         .requestMatchers(HttpMethod.POST, this.baseUrl + "/users").permitAll() // Protect the endpoint.
                         .requestMatchers(HttpMethod.PUT, this.baseUrl + "/users/**").permitAll() // Protect the endpoint.
